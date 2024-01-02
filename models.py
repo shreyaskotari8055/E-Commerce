b9bb397b0f989866d1998479695595d8b463d1d7
shreyaskotari8055 <shreyaskotari8055@gmail.com>
2024-01-02 16:55:01 +0530

    E-commerce

---
+++ models.py
@@ -0,0 +1,15 @@
+from tortoise import Model, fields
+from pydantic import BaseModel
+from datetime import datetime
+
+class User(Model):
+    id =  fields.IntField(pk= True, index= True)
+    username = fields.CharField(max_length= 20,null= False,unique= True)
+    email = fields.CharField(max_length= 200,null= False, unique= True)
+    password = fields.CharField(max_length= 16, null= False)
+    is_verified = fields.BooleanField(default= False)
+    join_date = fields.DateField(default= datetime.utcnow)
+
+
+class Business(Model):
+    
